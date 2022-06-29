7c12293927c3cf9567292fdea4a21e3f44bcf7cb
PradeepaVenkatesan <107341304+PradeepaVenkatesan@users.noreply.github.com>
2022-06-29 22:27:37 +0530

    Add files via upload

---
+++ language/Language.java
@@ -0,0 +1,5 @@
+package org.language;
+
+public interface Language {
+	public void Java(); 
+}
---
+++ language/MultipleLanguage.java
@@ -0,0 +1,10 @@
+package org.language;
+
+public abstract class MultipleLanguage {
+	public void python() {
+		System.out.println("Class Python");
+	}
+	public void ruby() {
+	} 
+
+}
